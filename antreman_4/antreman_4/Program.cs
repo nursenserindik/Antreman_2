a1058c46ea6afc2d9ca1237b921b8e125e123c16
nursenserindik <121162673+nursenserindik@users.noreply.github.com>
2024-08-18 13:49:08 +0300

    update

--- antreman_4/antreman_4/Program.cs
+++ antreman_4/antreman_4/Program.cs
@@ -6,8 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+
 
 
         }
