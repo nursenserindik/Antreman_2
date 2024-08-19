66c63f1ffa809b4c05886a4e49a8b706170a36bc
nursenserindik <121162673+nursenserindik@users.noreply.github.com>
2024-08-19 11:35:36 +0300

    update

--- antreman_4/antreman_4/Program.cs
+++ antreman_4/antreman_4/Program.cs
@@ -1,16 +1,48 @@
-﻿namespace antreman_4
+﻿using System;
+
+namespace finaSınav
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-
-           
-
-
+            Console.WriteLine(sifreOlustur("aslkaldslfa"));
+            Console.Read();
         }
+
+        static string sifreOlustur(string sifre)
+        {
+            string tersMetin = tersCevir(sifre);
+
+            string yeniSifre = "";
+
+            foreach (char harf in tersMetin)
+            {
+                if (harf == 'a')
+                    yeniSifre += '?';
+                else if (harf == 'e')
+                    yeniSifre += '*';
+                else if (harf == 'i' || harf == 'ı')
+                    yeniSifre += '=';
+                else if (harf == 'ö' || harf == 'o')
+                    yeniSifre += '&';
+                else if (harf == 'u' || harf == 'ü')
+                    yeniSifre += '+';
+                else
+                    yeniSifre += harf;
+            }
+
+            return yeniSifre;
+        }
+        static string tersCevir(string sifre)
+        {
+            string tersHali = "";
+
+            for (int x = sifre.Length - 1; x >= 0; x--)
+                tersHali += sifre[x];
+
+            return tersHali;
+        }
+
     }
 }
